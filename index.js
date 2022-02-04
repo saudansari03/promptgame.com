dc3d124fec35d3a33cacb09622a0a121b97b7811
saudansari03 <asaud984@gmail.com>
2022-02-04 15:22:51 +0530

    my guessing game

---
+++ index.js
@@ -0,0 +1,10 @@
+let input = prompt("say something to get out of the loop");
+while (input !== 'saud ansari') {
+    input = prompt("plzz try again")
+    if (input === 'saud ansari') {
+        break;
+    }
+    else if (input !== 'saud ansari') {
+    }
+}
+console.log("you win Boy")
